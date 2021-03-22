fa3b7eddb59cc8c7571a7d5b3e92a75859f3978b
colin colraine <colinco83@hotmail.co.uk>
2021-03-22 16:34:18 +0000

    another git 35

--- users/routes.js
+++ users/routes.js
@@ -8,9 +8,9 @@ var path = require('path')
 
 router.get('/dashboard', (req, res) => {
     //res.render(path.join(__dirname, 'dashboard.html'))
-    //res.sendFile(path.join(process.cwd() + '/dashboard.html'))user.get("email")
+    //res.sendFile(path.join(process.cwd() + '/dashboard.html'))
     //res.render('dashboard')
-    res.render('dashboard', {email: "john@gmail.com"})
+    res.render('dashboard', {firstName: 'Colin'})
 })
 
 router.post('/login',
@@ -201,7 +201,7 @@ function giveAccess(req, res, next){
                 //res.redirect('/users/' + UserId)
 
                 //res.redirect('/users/dashboard')
-                res.redirect('/users/dashboard?email='+email)
+                res.redirect('/users/dashboard?firstName='+firstName)
                 //res.redirect('/users/dashboard')
             }else{
                 res
